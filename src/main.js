3b56857edb61dd739028b30cb36f2613cadb73f1
Ivisonj <ivison_j@outlook.com>
2023-02-28 12:57:55 -0300

    Implementação JS

--- src/main.js
+++ src/main.js
@@ -1,9 +1,38 @@
-const whiteKey = document.querySelector('#naturais')
-// whiteKey.style.backgroundColor = "red"
-console.log('passei por aqui...')
-whiteKey.onmousemove = e => {
-    console.log('passei por aqui...')
-    if(e.buttons) {
-        console.log(e)
-    }
+//get all keys
+const keys = document.querySelectorAll('[data-key]')
+
+// console.log('Se liga...', keys)
+//play notes
+function playNote(event) {
+
+    let audioKeyCode = getKeyCode(event)
+    console.log(audioKeyCode)
+
+    //type or pressed key
+
+    //if key exists
+
+    //play audio
 }
+
+function getKeyCode(event) {
+    //key code
+    let keyCode
+    const isKeyboard = event.type === "keydown"
+   
+    if(isKeyboard) {
+        keyCode = event.keyCode
+    } else {
+           keyCode = event.target.dataset.key
+    }
+
+    return keyCode
+}
+
+//click with mouse
+keys.forEach((key) => {
+    key.addEventListener("click", playNote)
+})
+
+//keyboard type
+window.addEventListener("keydown", playNote)
